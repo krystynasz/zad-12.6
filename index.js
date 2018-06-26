327456ba51625e2bfcb06a8b41e1bd8391f3c2bf
krystynaszur <39401289+krystynaszur@users.noreply.github.com>
2018-06-26 21:55:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ document.getElementById('search').addEventListener("click", searchCountries);
 function showCountriesList(resp) {
     countriesList.innerHTML = '';
     resp.forEach(function(item) {
-       var liEl = document.createElement('li');
+       var liEl = document.createElement('td');
         liEl.innerText = item.name;
         countriesList.appendChild(liEl);
-        var capital = document.createElement('li');
+        var capital = document.createElement('td');
         capital.innerText = "Capital: " + item.capital;
         countriesList.appendChild(capital);
-        var population = document.createElement('li');
+        var population = document.createElement('td');
         population.innerText = "Population: " + item.population;
         countriesList.appendChild(population);
     });
